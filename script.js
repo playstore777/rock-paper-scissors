e06f6c16c2cd6a26acdf8d3f2ed4e8118d4daa0f
Play Store <ps494122@gmail.com>
2022-10-17 08:42:48 +0530

    Fix bugs in Invalid input validation

--- script.js
+++ script.js
@@ -1,43 +1,41 @@
 function game() {
-    let playerScore = 0;
-    let computerScore = 0;
+    let playerScore = score.playerScore;
+    let computerScore = score.computerScore;
     for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt('Please type between Rock, Paper and Scissors: ').toLowerCase();
-        if (playerSelection != 'rock' || playerSelection != 'paper' || playerSelection != 'scissors') {
-            console.log('Please enter a valid option. Valid Options are ROCK, PAPER & SCISSORS')
-            return;
+        playerSelection = prompt('Please type between Rock, Paper and Scissors: ').toLowerCase();
+        if (playerSelection != 'rock' && playerSelection != 'paper' && playerSelection != 'scissors') {
+            console.log('Please enter a valid option. Valid Options are ROCK, PAPER & SCISSORS');
+            i--; // replay same round!
+            continue;
         }
-        let computerSelection = getComputerChoice();
-        let results = playRound(playerSelection, computerSelection, playerScore, computerScore);
-        playerScore = results.playerScore;
-        computerScore = results.computerScore;
+        score = playRound(playerSelection, computerSelection, playerScore, computerScore);
     }
-
     return playerScore > computerScore ? "You Win!" : playerScore < computerScore ? "You Lose!" : "It's a tie";
 }
 
-function playRound(playerSelection, computerSelection, playerScore, computerScore) {
-    console.log(playerScore, computerScore);
+function playRound(playerSelection, computerSelection) {
+    console.log(score.playerScore, score.computerScore);    
+
     if (playerSelection === 'rock' && computerSelection === 'paper' ||
         playerSelection === 'paper' && computerSelection === 'scissors' ||
         playerSelection === 'scissors' && computerSelection === 'rock') {
         return {
-            computerScore: (computerScore + 1),
-            playerScore: (playerScore)
+            'computerScore': (score.computerScore + 1),
+            'playerScore': (score.playerScore)
         };
         // return `You Lose! ${computerSelection} beats ${playerSelection}`;
     } else if (playerSelection === 'paper' && computerSelection === 'rock' ||
         playerSelection === 'scissors' && computerSelection === 'paper' ||
         playerSelection === 'rock' && computerSelection === 'scissors') {
         return {
-            computerScore: computerScore,
-            playerScore: (playerScore + 1)
+            'computerScore': score.computerScore,
+            'playerScore': (score.playerScore + 1)
         }
         // return `You Win! ${playerSelection} beats ${computerSelection}`;
     }
     return {
-        computerScore: (computerScore + 1),
-        playerScore: (playerScore + 1)
+        'computerScore': (score.computerScore + 1),
+        'playerScore': (score.playerScore + 1)
     }
     // return "It's a tie!";
 }
@@ -51,5 +49,12 @@ function randomNumberGeneratorBetween(min, max) {
     let random = Math.random();
     return Math.round(random * (max - min)) + min;
 }
+
+let playerSelection = "";
+let computerSelection = getComputerChoice();
+let score = {
+    'playerScore': 0,
+    'computerScore': 0
+};
 
 console.log(game());
